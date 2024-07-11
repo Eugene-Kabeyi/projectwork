b2c4eeb59a8b11430d216562f52ad611c80826f7
Eugene-Kabeyi <eugenekabeyi144@gmail.com>
2024-07-11 01:40:35 -0700

    Added on groceries website

---
+++ groceries/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 10000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
